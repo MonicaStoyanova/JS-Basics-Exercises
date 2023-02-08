76e573ca866661403ebc326e78289c451a05d935
Monica Stoyanova <114282401+MonicaStoyanova@users.noreply.github.com>
2023-02-08 12:05:27 +0200

    Delete 1stDayLab directory

--- 1stDayLab/inchesToCm.js
+++
@@ -1,9 +0,0 @@
-function inches(input){
-
-    let a = Number(input[0]);
-
-    let b = a * 2.54;
-
-    console.log(b);
-}
-inches(["7"])
--- 1stDayLab/project1.js
+++
@@ -1,9 +0,0 @@
-function hello() {
-    
-    let firstNumber = 7;
-    let secondNumber = 2;
-    
-let sum = firstNumber % secondNumber;
-console.log(sum)
-}
-hello();
