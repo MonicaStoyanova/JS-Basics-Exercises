a628822869d476b23b40facbbc73b915f0334571
Monica Stoyanova <114282401+MonicaStoyanova@users.noreply.github.com>
2023-02-08 12:08:43 +0200

    Delete 2ndDayExercise directory

--- 2ndDayExercise/radiansToDegrees.js
+++
@@ -1,6 +0,0 @@
-function radiansToDegrees(input){
-let radians = Number(input[0])
-let degrees = radians*180 / Math.PI;
-console.log(degrees)
-}
-radiansToDegrees(["3.1416"])
--- 2ndDayExercise/usdToBgn.js
+++
@@ -1,6 +0,0 @@
-function convertot(input) {
-    let usd = Number(input[0]);
-    let bgn = usd * 1.79549;
-    console.log(bgn);
-  }
-  convertor(['22'])
